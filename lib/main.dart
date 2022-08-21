befaa3848c522674983193996e08aca58c9d4e2d
youms <yussef.msabeh@gmail.com>
2022-08-21 11:40:28 +0300

    remove debuger bar

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
           splitScreenMode: true,
           builder: (context, child) {
             return MaterialApp(
+              debugShowCheckedModeBanner: false,
               home: CatScreen(),
             );
           }),
